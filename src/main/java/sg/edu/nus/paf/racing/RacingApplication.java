96020704d7eaabe2081448c56f58d38fb6004723
zawan1997 <nawaz.shss@gmail.com>
2022-12-02 11:38:08 +0800

    first

---
+++ src/main/java/sg/edu/nus/paf/racing/RacingApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.paf.racing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/paf/racing/RacingApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.paf.racing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
